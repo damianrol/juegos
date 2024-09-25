4bde27ab50cd20623edb63bc6b4d88eeb574151d
damianrol <dgroldamian@gmail.com>
2024-09-25 11:33:23 -0300

    libro 2 html

--- js/footer.js
+++ js/footer.js
@@ -1,7 +1,8 @@
 //Me creo la variable miFooter y pegamos el código que tenemos en el HTML
 
 let miFooter=`
-<p>2024. SmartTEAM</p>
+<img src="./imagenes/logo-SmartTeam+bajada-alta.png" alt="logo" width=58>
+<p>® 2024 , SmartTEAM</p>
 `
 
 //Selecciono el footer y con innerHTML agrego lo que definí en la variable miFooter
